74ed72e178464712e9eb0bc9179f94e22b23a286
Pavan Bhat <122112767+PavanAgasal@users.noreply.github.com>
2023-06-17 21:52:05 +0530

    DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,9 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    public DeluxePizza(Boolean isVeg) {
+    public DeluxePizza(boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        this.addExtraCheese();
+        this.addExtraToppings();
     }
 }
